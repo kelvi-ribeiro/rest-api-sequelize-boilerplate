dd198c50e3c1f551a553d5c3a9fa082dad0340d4
Kelvi Ribeiro <kelvi.ribeiro@gmail.com>
2019-05-05 12:04:28 -0300

    Criando o Recurso de Post
    Endpoint de get possui defeitos quando há o atributo order by com o mesmo nome das duas entidades, na de relacionamento e na própria

---
+++ resources/postResource.js
@@ -0,0 +1,52 @@
+const base_url = '/posts'
+
+module.exports = (app, db) => {  
+
+  app.get(`${base_url}`, (req, res) => {        
+    const { Op } = db.Sequelize
+    const options = {
+      attributes: ['id', 'title'],
+      include: [{
+        model: db.author,
+        as: 'author', // specifies how we want to be able to access our joined rows on the returned data,
+        attributes: ['firstName'] 
+      }],
+      page: req.query.page, 
+      paginate: +req.query.pageSize, 
+      order: [[req.query.orderBy || db.sequelize.models.post.rawAttributes[Object.keys(db.sequelize.models.post.rawAttributes)[0]], req.query.direction || 'ASC']],
+      [req.query.campo && req.query.condicao ? 'where':'']: { [req.query.campo || '']: { [Op.like]: `%${req.query.condicao|| ''}%` } }
+    }
+    db.post.paginate(options).then(result => res.json(result))
+  });
+
+  app.get(`${base_url}/:id`, (req, res) =>
+    db.post.findByPk(req.params.id).then((result) => res.json(result))
+  );
+
+  app.post(`${base_url}`, (req, res) =>
+    db.post.create({
+      title: req.body.title,
+      content: req.body.content
+    }).then((result) => res.json(result))
+  );
+
+  app.put(`${base_url}/:id`, (req, res) =>
+    db.post.update({
+      title: req.body.title,
+      content: req.body.content
+    },
+      {
+        where: {
+          id: req.params.id
+        }
+      }).then((result) => res.json(result))
+  );
+
+  app.delete(`${base_url}/:id`, (req, res) =>
+    db.post.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then((result) => res.json(result))
+  );
+}
