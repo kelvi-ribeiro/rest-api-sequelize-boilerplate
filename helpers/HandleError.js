3f8636acd9c4dcbd171e7b5e39652e02d935c1df
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-06-14 15:06:45 -0300

    Corrigindo função notFoundError
    Colocando status code 404 para a função NotFoundError

--- helpers/HandleError.js
+++ helpers/HandleError.js
@@ -1,7 +1,7 @@
 var errors = require('restify-errors');
 
-function notFoundError(response,id,rotulo,message){
-    return response.json(new errors.NotFoundError(`Elemento com ${rotulo || 'Identificador'} ${id} ${message || 'Não encontrado'}`));
+function notFoundError(response,id,rotulo,message){    
+    return response.status(404).json(new errors.NotFoundError(`Elemento com ${rotulo || 'Identificador'} ${id} ${message || 'Não encontrado'}`)    );
 }
 
 module.exports = {notFoundError}
